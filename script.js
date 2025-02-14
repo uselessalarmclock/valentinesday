9586067dd17bb5c8d2276dcb3446a1183097daee
uselessalarmclock <lglittle07@gmail.com>
2025-02-14 16:13:42 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -15,18 +15,18 @@ function sayYes() {
     const container = document.querySelector(".container");
     container.innerHTML = ""; // Remove everything inside
 
-    // Create a new message
+    
     const message = document.createElement("h1");
     message.innerText = "Yay! Can't wait to spend time with you!!! 💖";
     container.appendChild(message);
 
-    // Create an image element for the GIF
+    
     const gif = document.createElement("img");
     gif.src = "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExZjdmYnoxNTVwcmo4bmpmY3ZtMGI1bGk5YnBrOXFteTRneWYwdzMwaCZlcD12MV9naWZzX3NlYXJjaCZjdD1n/Ie4CIIvQS0bk3zwZlM/giphy.gif"; // Replace with your own GIF URL
     gif.style.width = "300px";
     gif.style.marginTop = "20px";
 
-    // Add the GIF to the page
+    
     container.appendChild(gif);
 }
 
